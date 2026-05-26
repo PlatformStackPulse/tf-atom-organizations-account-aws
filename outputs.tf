f21aded0fb3a2b1624ea852f4b0f5a2ca3024ab1
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-27 00:14:51 +0100

    feat: implement tf-atom-organizations-account-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
-output "enabled" {
-  description = "Whether the module is enabled."
-  value       = local.enabled
+output "id" {
+  description = "ID of the account"
+  value       = try(aws_organizations_account.this[0].id, null)
+}
+
+output "arn" {
+  description = "ARN of the account"
+  value       = try(aws_organizations_account.this[0].arn, null)
+}
+
+output "name" {
+  description = "Name of the account"
+  value       = try(aws_organizations_account.this[0].name, null)
 }
